68c3f46b2be6de23ae5edcd6d2a1727c01f33f1e
jzs27 <jzs27@cornell.edu>
2021-07-13 12:17:35 -0500

    Implemented a camera/photo library feature

---
+++ FBUApp/View Controllers/Profile View Controllers/VehicleRegistrationViewController.h
@@ -0,0 +1,16 @@
+//
+//  VehicleRegistrationViewController.h
+//  FBUApp
+//
+//  Created by jessicasyl on 7/13/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VehicleRegistrationViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FBUApp/View Controllers/Profile View Controllers/VehicleRegistrationViewController.m
@@ -0,0 +1,79 @@
+//
+//  VehicleRegistrationViewController.m
+//  FBUApp
+//
+//  Created by jessicasyl on 7/13/21.
+//
+
+#import "VehicleRegistrationViewController.h"
+#import <Parse/Parse.h>
+#import "Vehicle.h"
+
+@interface VehicleRegistrationViewController ()<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *vehicleView;
+
+@end
+
+@implementation VehicleRegistrationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)didTapVehicle:(id)sender {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+
+    // The Xcode simulator does not support taking pictures, so let's first check that the camera is indeed supported on the device before trying to present it.
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    // Get the image captured by the UIImagePickerController
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    //UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    CGSize size = CGSizeMake(1500, 1500);
+    self.vehicleView.image = [self resizeImage:originalImage withSize:size];
+    
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
