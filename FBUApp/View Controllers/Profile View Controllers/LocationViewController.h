bb114e234a962c66a772282fc697fb6f855c27d5
jzs27 <jzs27@cornell.edu>
2021-07-16 15:04:09 -0500

    Implemented markers for different cities.

--- FBUApp/View Controllers/Profile View Controllers/LocationViewController.h
+++ FBUApp/View Controllers/Profile View Controllers/LocationViewController.h
@@ -11,6 +11,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface LocationViewController : UIViewController
 
+@property (strong, nonatomic) NSString *location;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- FBUApp/View Controllers/Profile View Controllers/LocationViewController.m
+++ FBUApp/View Controllers/Profile View Controllers/LocationViewController.m
@@ -25,10 +25,14 @@
                                                                zoom:5];
   GMSMapView *mapView = [GMSMapView mapWithFrame:self.view.frame camera:camera];
   mapView.myLocationEnabled = YES;
-  [self.view addSubview:mapView];
+    mapView.settings.myLocationButton = YES;
+  //[self.view addSubview:mapView];
     
     CLLocationCoordinate2D houston = CLLocationCoordinate2DMake(29.760427, -95.369803);
     CLLocationCoordinate2D austin = CLLocationCoordinate2DMake(30.267153, -97.743061);
+    CLLocationCoordinate2D dallas = CLLocationCoordinate2DMake(32.776664, -96.796988);
+    
+    CLLocationCoordinate2D sanAntonio = CLLocationCoordinate2DMake(29.422122, -98.493628);
 
   // Creates a marker in the center of the map.
   GMSMarker *marker = [[GMSMarker alloc] init];
@@ -42,6 +46,18 @@
       marker2.title = @"Austin";
       marker2.snippet = @"Texas";
       marker2.map = mapView;
+    
+    GMSMarker *marker3 = [[GMSMarker alloc] init];
+      marker3.position = dallas;
+      marker3.title = @"Dallas";
+      marker3.snippet = @"Texas";
+      marker3.map = mapView;
+    
+    GMSMarker *marker4 = [[GMSMarker alloc] init];
+      marker4.position = sanAntonio;
+      marker4.title = @"San Antonio";
+      marker4.snippet = @"Texas";
+      marker4.map = mapView;
     
 //  marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
 //  marker.title = @"Sydney";
