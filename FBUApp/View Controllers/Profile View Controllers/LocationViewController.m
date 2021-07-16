824ac0970bce200a04e5fd8bf43673885faeefde
jzs27 <jzs27@cornell.edu>
2021-07-16 15:34:57 -0500

    Cleaned code and transferred map view from full screen into smaller view.

--- FBUApp/View Controllers/Profile View Controllers/LocationViewController.m
+++ FBUApp/View Controllers/Profile View Controllers/LocationViewController.m
@@ -11,22 +11,21 @@
 
 @interface LocationViewController ()
 
+@property (weak, nonatomic) IBOutlet GMSMapView *mapView;
+
 @end
 
 @implementation LocationViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-  // Do any additional setup after loading the view.
-  // Create a GMSCameraPosition that tells the map to display the
-  // coordinate -33.86,151.20 at zoom level 6.
+  
   GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:31.968599
                                                           longitude:-99.901813
                                                                zoom:5];
-  GMSMapView *mapView = [GMSMapView mapWithFrame:self.view.frame camera:camera];
-  mapView.myLocationEnabled = YES;
-    mapView.settings.myLocationButton = YES;
-  //[self.view addSubview:mapView];
+  
+    [self.mapView setCamera:camera];
+  
     
     CLLocationCoordinate2D houston = CLLocationCoordinate2DMake(29.760427, -95.369803);
     CLLocationCoordinate2D austin = CLLocationCoordinate2DMake(30.267153, -97.743061);
@@ -34,35 +33,32 @@
     
     CLLocationCoordinate2D sanAntonio = CLLocationCoordinate2DMake(29.422122, -98.493628);
 
-  // Creates a marker in the center of the map.
+  
   GMSMarker *marker = [[GMSMarker alloc] init];
     marker.position = houston;
     marker.title = @"Houston";
     marker.snippet = @"Texas";
-    marker.map = mapView;
+    marker.map = self.mapView;
     
     GMSMarker *marker2 = [[GMSMarker alloc] init];
       marker2.position = austin;
       marker2.title = @"Austin";
       marker2.snippet = @"Texas";
-      marker2.map = mapView;
+      marker2.map = self.mapView;
     
     GMSMarker *marker3 = [[GMSMarker alloc] init];
       marker3.position = dallas;
       marker3.title = @"Dallas";
       marker3.snippet = @"Texas";
-      marker3.map = mapView;
+      marker3.map = self.mapView;
     
     GMSMarker *marker4 = [[GMSMarker alloc] init];
       marker4.position = sanAntonio;
       marker4.title = @"San Antonio";
       marker4.snippet = @"Texas";
-      marker4.map = mapView;
+      marker4.map = self.mapView;
     
-//  marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
-//  marker.title = @"Sydney";
-//  marker.snippet = @"Australia";
-//  marker.map = mapView;
+
 }
 
 @end
