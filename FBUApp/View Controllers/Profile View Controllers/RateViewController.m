28adf0e88d15460b1fc0722ded98510854ba72d6
jzs27 <jzs27@cornell.edu>
2021-07-30 18:39:38 -0500

    Implemented neural network for rate algorithm.

--- FBUApp/View Controllers/Profile View Controllers/RateViewController.m
+++ FBUApp/View Controllers/Profile View Controllers/RateViewController.m
@@ -24,6 +24,8 @@
 @property NSDictionary *makes;
 @property NSDictionary *types;
 @property NSDictionary *year;
+@property int result;
+@property SNTrainingRecord *record;
 
 @end
 
@@ -105,7 +107,6 @@
             [self performSegueWithIdentifier:@"fromRate" sender:nil];
         }
     }];
-    
 }
 
 #pragma mark - Navigation
@@ -123,31 +124,84 @@
 
 -(void)createNeuralNet{
     SNTrainingRecord records[] = {
-        {SNInput(1,1,0), SNOutput(1)},
-        {SNInput(0,1,1), SNOutput(1)},
-        {SNInput(1,0,1), SNOutput(1)},
-        {SNInput(1,0,0), SNOutput(0)},
-        {SNInput(0,1,0), SNOutput(0)},
-        {SNInput(0,0,1), SNOutput(0)},
-        {SNInput(1,1,1), SNOutput(1)},
-        {SNInput(0,0,0), SNOutput(0)}
+        {SNInput(1,1,1,1,1), SNOutput(1)},
+        {SNInput(1,1,1,1,0), SNOutput(1)},
+        {SNInput(1,1,1,0,1), SNOutput(1)},
+        {SNInput(1,1,0,1,1), SNOutput(1)},
+        {SNInput(1,0,1,1,1), SNOutput(1)},
+        {SNInput(0,1,1,1,1), SNOutput(1)},
+        {SNInput(1,1,1,0,0), SNOutput(1)},
+        {SNInput(1,1,0,1,0), SNOutput(1)},
+        {SNInput(1,0,1,1,0), SNOutput(1)},
+        {SNInput(0,1,1,1,0), SNOutput(1)},
+        {SNInput(1,1,0,0,1), SNOutput(1)},
+        {SNInput(1,0,1,0,1), SNOutput(1)},
+        {SNInput(0,1,1,0,1), SNOutput(1)},
+        {SNInput(1,0,0,1,1), SNOutput(1)},
+        {SNInput(0,1,0,1,1), SNOutput(1)},
+        {SNInput(0,0,1,1,1), SNOutput(1)},
+        {SNInput(0,0,0,0,0), SNOutput(0)},
+        {SNInput(0,0,0,0,1), SNOutput(0)},
+        {SNInput(0,0,0,1,0), SNOutput(0)},
+        {SNInput(0,0,1,0,0), SNOutput(0)},
+        {SNInput(0,1,0,0,0), SNOutput(0)},
+        {SNInput(1,0,0,0,0), SNOutput(0)},
+        {SNInput(0,0,0,1,1), SNOutput(0)},
+        {SNInput(0,0,1,0,1), SNOutput(0)},
+        {SNInput(0,1,0,0,1), SNOutput(0)},
+        {SNInput(1,0,0,0,1), SNOutput(0)},
+        {SNInput(0,0,1,1,0), SNOutput(0)},
+        {SNInput(0,1,0,1,0), SNOutput(0)},
+        {SNInput(1,0,0,1,0), SNOutput(0)},
+        {SNInput(0,1,1,0,0), SNOutput(0)},
+        {SNInput(1,0,1,0,0), SNOutput(0)},
+        {SNInput(1,1,0,0,0), SNOutput(0)}
     };
 
     SNNeuralNet *net = [[SNNeuralNet alloc] initWithTrainingData:records
-                                                      numRecords:8
-                                                       numInputs:3
+                                                      numRecords:31
+                                                       numInputs:5
                                                       numOutputs:1];
-
-    
     
     net.maxIterations = 20000;  // maximum training iterations
     net.minError = 0.0001;       // error threshold to reach
     net.learningRate = 0.03;     // influences how quickly the network trains
     net.momentum = 0.1;         // influences learning rate
     
-    
     double error = [net train:records numRecords:8];
-    double *output = [net runInput:SNInput(1, 0,1)];
+    
+    int location = 0;
+    if ([self.vehicle.location containsString:@"NY"] || [self.vehicle.location containsString:@"CA"]){
+        location = 1;
+    }
+    NSTimeInterval secondsBetween = [self.vehicle.availableStartDate timeIntervalSinceDate:self.vehicle.availableEndDate];
+    int days = secondsBetween / 86400;
+    int time = 0;
+    if (days < 7){
+        time = 1;
+    }
+    
+    int make = 0;
+    if ([self.makes objectForKey:self.vehicle.make]){
+        make =1;
+    }
+    
+    int type = 0;
+    if (![self.vehicle.type  isEqual: @"Small to Full Size"]){
+        type = 1;
+    }
+    
+    int year = 0;
+    if ([self.vehicle.year  isEqual: @"2021"] || [self.vehicle.year  isEqual: @"2020"] || [self.vehicle.year  isEqual: @"2019"]){
+        year = 1;
+    }
+    
+    double *output = [net runInput:SNInput(make, year, type, location, time)];
+    
+    self.result = 0;
+    if (output[0] >= 0.9){
+        self.result = 1;
+    }
 }
 
 @end
