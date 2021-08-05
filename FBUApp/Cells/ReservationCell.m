68c1ba58f48d4141584adb0d5d62d72c8a45b1c2
jzs27 <jzs27@cornell.edu>
2021-08-05 11:02:34 -0500

    Updated UI for Reservations view.

--- FBUApp/Cells/ReservationCell.m
+++ FBUApp/Cells/ReservationCell.m
@@ -31,7 +31,7 @@
     self.locationLabel.text = self.reservation.vehicle.location;
     self.vehicleInfoLabel.text = [NSString stringWithFormat:@"%@ %@ %@", self.reservation.vehicle.make,self.reservation.vehicle.model,self.reservation.vehicle.year ];
     self.dateLabel.text = [Reservation createDateString:self.reservation.startRentDate withEndDate:self.reservation.endRentDate];
-    self.priceLabel.text = [NSString stringWithFormat:@"$%@ /day",self.reservation.vehicle.rate];
+    self.priceLabel.text = [NSString stringWithFormat:@"$%@ / day",self.reservation.vehicle.rate];
 }
 
 @end
